bb1ee9846556238f306f0476e45df42e38aa3270
Wiktor Kruszewicz <wiktorkruszewicz@gmail.com>
2023-01-05 00:04:30 +0100

    Added Card Stack class and basic tests

---
+++ test_card_stack.py
@@ -0,0 +1,16 @@
+from card_stack import CardStack
+from card import Card, Suits
+
+
+def test_card_stack_init():
+    stack = CardStack()
+    assert len(stack.cards) == 0
+
+
+def test_card_stack_add_card():
+    stack = CardStack()
+    two_of_spades = Card(Suits.SPADES, 2)
+    stack.add_card_on_top(two_of_spades)
+    assert len(stack.cards) == 1
+    assert two_of_spades in stack.cards
+    assert stack.top_card() == two_of_spades
