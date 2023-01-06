2071920d5be624b052b7c937491db5b1a766d989
Wiktor Kruszewicz <wiktorkruszewicz@gmail.com>
2023-01-06 17:58:41 +0100

    Added the str functionality to card stack

--- test_card_stack.py
+++ test_card_stack.py
@@ -13,4 +13,11 @@ def test_card_stack_add_card():
     stack.add_card_on_top(two_of_spades)
     assert len(stack.cards) == 1
     assert two_of_spades in stack.cards
-    assert stack.top_card() == two_of_spades
+    assert stack.top_card == two_of_spades
+
+
+def test_card_stack_str():
+    stack = CardStack()
+    two_of_spades = Card(Suits.SPADES, 2)
+    stack.add_card_on_top(two_of_spades)
+    assert str(stack) == 'The card at the top is Two of Spades'
