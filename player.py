8e734812003308ed92aa66c5ab3fb7c219049876
alicja <alicjakruszewicz3@gmail.com>
2024-11-30 16:56:59 +0100

    Added documentation to the player class

--- player.py
+++ player.py
@@ -8,27 +8,42 @@ class WrongCardIndexError(Exception):
 
 
 class Player:
+    """
+    Class Player. Contains attributes:
+
+    :param name: The player's name, used for aesthetic purposes
+    :type name: str
+
+    :param hand: Player's hand of cards
+    :type hand: List[Card]
+    """
     def __init__(self, name: str, cards: Optional[List[Card]] = None):
+        """Initialises the player class"""
         if not cards:
-            self._cards = []
+            self._hand = []
         else:
-            self._cards = cards
+            self._hand = cards
 
         self._name = name
 
     @property
-    def cards(self) -> List[Card]:
-        return self._cards
+    def hand(self) -> List[Card]:
+        return self._hand
 
     @property
     def name(self):
         return self._name
 
     def add_card(self, card: 'Card') -> None:
-        self._cards.append(card)
+        """Adds a card to the player's hand"""
+        self._hand.append(card)
 
     def remove_card(self, card_index: int) -> 'Card':
+        """
+        Removes a card from the player's hand
+        Throws error if given an invalid index
+        """
         try:
-            return self._cards.pop(card_index)
+            return self._hand.pop(card_index)
         except IndexError:
             raise WrongCardIndexError()
